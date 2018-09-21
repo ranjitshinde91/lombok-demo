528568f2d1a36a4213f02f300bc5d3974674a7df
Ranjit <ranjitshinde91@gmail.com>
2018-09-21 12:48:14 +0530

    builder with singular

--- src/main/java/builder/BuilderExample.java
+++ src/main/java/builder/BuilderExample.java
@@ -2,20 +2,34 @@ package builder;
 
 
 import lombok.Builder;
+import lombok.Singular;
 import lombok.Value;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Builder
 @Value
 class Student {
     private String firstName;
     private String lastName;
+    @Singular
+    private List<String> addresses;
 }
 
 public class BuilderExample {
     public static void main(String[] args) {
-        Student student = Student.builder().firstName("Ranjit").lastName("Shinde").build();
+        List<String> addresses = Arrays.asList("Pune", "Sangvi");
+        Student student = Student.builder().firstName("Ranjit")
+                .lastName("Shinde")
+                .address("Pune")
+                .addresses(addresses)
+                .build();
+
         System.out.println(student);
         student.getFirstName();
-        System.out.println(student);
+        System.out.println(student.getAddresses());
+
+
     }
 }
